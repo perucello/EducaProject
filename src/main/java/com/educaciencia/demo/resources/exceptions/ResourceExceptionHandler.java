1848a6f2fc0b4c287f9692c775bfc7f956a4701f
perucello <perucello@hotmail.com>
2020-05-20 11:43:42 -0300

    Criado Exception handling - DELETE

--- src/main/java/com/educaciencia/demo/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/educaciencia/demo/resources/exceptions/ResourceExceptionHandler.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.educaciencia.demo.services.exceptions.DatabaseException;
 import com.educaciencia.demo.services.exceptions.ResourceNotFoundException;
 
 //Interceptar as excessoes para o obj executar tratamento
@@ -22,4 +23,18 @@ public class ResourceExceptionHandler {
 		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
 		return ResponseEntity.status(status).body(err);
 	}
+	
+	
+	
+	@ExceptionHandler(DatabaseException.class)
+	public ResponseEntity<StandardError> dataBase(DatabaseException e, HttpServletRequest request){
+		String error = "DataBase error";
+		HttpStatus status = HttpStatus.BAD_REQUEST;
+		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+	}
+	
+	
+	
+	
 }
