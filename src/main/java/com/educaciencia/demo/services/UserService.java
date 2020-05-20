33f4f58a7e69b40b87d259293bcc5d837f951a14
perucello <perucello@hotmail.com>
2020-05-20 11:50:53 -0300

    Criado Exception handling - UPDATE

--- src/main/java/com/educaciencia/demo/services/UserService.java
+++ src/main/java/com/educaciencia/demo/services/UserService.java
@@ -3,6 +3,8 @@ package com.educaciencia.demo.services;
 import java.util.List;
 import java.util.Optional;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -49,9 +51,14 @@ public class UserService {
 	}
 	
 	public User update(Long id, User obj) {
+		try {
 		User entity = repository.getOne(id);//para monitorar o Objeto e depois efetuar operação no Banco de Dados(melhor nque FindAllId)
 		updateData(entity, obj);
 		return repository.save(entity);
+		}catch (EntityNotFoundException e) {
+			//e.printStackTrace();
+			throw new ResourceNotFoundException(id);
+		}
 	}
 
 	private void updateData(User entity, User obj) {
